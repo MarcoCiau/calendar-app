6d068deba0a633eda8cca2cab46a2131671b0d43
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-15 07:15:36 -0600

    defining calendar action methods

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,3 +3,13 @@ import { actionTypes } from "../actionTypes/actionTypes";
 export const openModalAction = { type: actionTypes.uiOpenModal };
 
 export const closeModalAction = { type: actionTypes.uiCloseModal };
+
+export const selectEventAction = (e) => ({
+  type: actionTypes.eventSelected,
+  payload: e,
+});
+
+export const addNewEventAction = (e) => ({
+  type: actionTypes.eventAddNew,
+  payload: e,
+});
