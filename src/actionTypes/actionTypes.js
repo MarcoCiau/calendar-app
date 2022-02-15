ed65c172ece3e2d6dc58f83c41e2489cf44d061d
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-15 07:14:45 -0600

    defining new actions types for calendar events

--- src/actionTypes/actionTypes.js
+++ src/actionTypes/actionTypes.js
@@ -1,4 +1,6 @@
 export const actionTypes = {
     uiOpenModal : "[ui] Open Modal Action",
     uiCloseModal : "[ui] Close Modal Action",
+    eventSelected : "[event] Select Event",
+    eventAddNew: "[event] Add new Event"
 }
