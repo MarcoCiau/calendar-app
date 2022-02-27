d35a499d95407c54e8f47bb0a76cd9b4d6efe836
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-27 12:19:17 -0600

    fix id

--- src/reducers/calendar.js
+++ src/reducers/calendar.js
@@ -4,28 +4,24 @@ import { actionTypes } from "../actionTypes/actionTypes";
 const initialState = {
   events: [
     {
-      id: new Date(),
+      _id: new Date(),
       title: "Develop Promatic API",
       notes:"",
       start: moment().toDate(),
       end: moment().add(2, "hours").toDate(),
-      allDay: false,
-      resource: false,
       user: {
         id: "oandnbf9ss0jk498685jhn",
         name: "marco",
       },
     },
     {
-      id: new Date() + 100,
+      _id: new Date() + 100,
       title: "Learn English",
       notes:"",
       start: moment().add(9, "hours").toDate(),
       end: moment().add(10, "hours").toDate(),
-      allDay: false,
-      resource: false,
       user: {
-        id: "oandnbf56s0jk498685jhn",
+        _id: "oandnbf56s0jk498685jhn",
         name: "user1",
       },
     },
