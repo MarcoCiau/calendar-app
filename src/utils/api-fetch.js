4feea102d637e47614246aaf632e2b3e734ca9ad
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-28 12:51:45 -0600

    GET request

--- src/utils/api-fetch.js
+++ src/utils/api-fetch.js
@@ -11,9 +11,25 @@ const executePostReq = async (token="", endpoint="", body={}) => {
         },
         body: JSON.stringify(body)
     });
-    return response.json();
+    const data = await response.json(); 
+    return data;
+}
+
+const executeGetRequest = async (endpoint="") => {
+    const token = localStorage.getItem("accessToken");
+    const response = await fetch(`${baseUrl}${endpoint}`, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'x-token': token
+        },
+    });
+    const data = await response.json();
+    return data;
 }
 
 export {
-    executePostReq
+    executePostReq,
+    executeGetRequest
 }
