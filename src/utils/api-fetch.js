0be8cd1040ab2bec3efe93bb33a82fac30490bad
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-28 13:38:10 -0600

    generic api request

--- src/utils/api-fetch.js
+++ src/utils/api-fetch.js
@@ -1,5 +1,22 @@
 const baseUrl = process.env.REACT_APP_API_URL;
 
+const executeAPIRequest = async (method="GET", endpoint="", body={}) => {
+    const token = localStorage.getItem("accessToken");
+    const response = await fetch(`${baseUrl}${endpoint}`, {
+        method: method,
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-token': token
+        },
+        body: JSON.stringify(body)
+    });
+    const data = await response.json(); 
+    return data;
+}
+
+
 const executePostReq = async (token="", endpoint="", body={}) => {
     const response = await fetch(`${baseUrl}${endpoint}`, {
         method: 'POST',
@@ -30,6 +47,7 @@ const executeGetRequest = async (endpoint="") => {
 }
 
 export {
+    executeAPIRequest,
     executePostReq,
     executeGetRequest
 }
