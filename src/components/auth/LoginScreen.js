2b3ee8a958f7738fffd8d4e952d73b23eeb9a8d5
MarcoCiau <marcoantoniociau@hotmail.com>
2022-03-30 23:39:18 -0600

    signup & login components

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,127 +1,74 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import Swal from "sweetalert2";
-import { authStartLoginAction, authStartRegisterAction } from "../../actions/actions";
+import { useNavigate } from "react-router-dom";
+import { authStartLoginAction } from "../../actions/actions";
 import { useForm } from "../../hooks/useForm";
-import "./LoginScreen.css";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [loginFormValues, handleInputChangeLoginForm] = useForm({
     loginEmail: "",
     loginPassword: "",
   });
-
-  const [registerFormValues, handleInputChangeRegisterForm ] = useForm({
-    registerEmail:"",
-    registerName:"",
-    registerPassword:"",
-    registerDuplicatedPassword:"",
-  });
-
   const { loginEmail, loginPassword } = loginFormValues;
-  const { registerEmail, registerName, registerPassword, registerDuplicatedPassword } = registerFormValues;
-
   const handleOnSubmit = (e) => {
     e.preventDefault();
     dispatch(authStartLoginAction(loginEmail, loginPassword));
   };
 
-  const handleOnSubmitRegister = (e) => {
+  const handleSignupNow = (e) => {
     e.preventDefault();
-    if (registerPassword !== registerDuplicatedPassword)
-    {
-      return Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: `The password are not equal`
-      });
-    }
-    dispatch(authStartRegisterAction(registerName, registerEmail, registerPassword));
-  }
+    navigate("/signup", {
+      replace: true,
+    });
+  };
 
   return (
-    <div className="container login-container">
-      <div className="row">
-        <div className="col-md-6 login-form-1">
-          <h3>Ingreso</h3>
-          <form onSubmit={handleOnSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Correo"
-                name="loginEmail"
-                value={loginEmail}
-                onChange={handleInputChangeLoginForm}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Contraseña"
-                name="loginPassword"
-                value={loginPassword}
-                onChange={handleInputChangeLoginForm}
-              />
-            </div>
-            <div className="form-group">
-              <input type="submit" className="btnSubmit" value="Login" />
-            </div>
-          </form>
-        </div>
+    <div className="text-center container col-md-6 mx-auto col-lg-3">
+      <div className=" mt-5 form-signin">
+        <form onSubmit={handleOnSubmit}>
+          <i className="fas fa-shield-alt fa-3x mb-3" />
+          <h1 className="h3 mb-3 fw-normal">Welcome Back</h1>
 
-        <div className="col-md-6 login-form-2">
-          <h3>Registro</h3>
-          <form onSubmit={handleOnSubmitRegister}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nombre"
-                name="registerName"
-                value={registerName}
-                onChange={handleInputChangeRegisterForm}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Correo"
-                name="registerEmail"
-                value={registerEmail}
-                onChange={handleInputChangeRegisterForm}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Contraseña"
-                name="registerPassword"
-                value={registerPassword}
-                onChange={handleInputChangeRegisterForm}
-              />
-            </div>
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="emailLogin"
+              placeholder="name@example.com"
+              name="loginEmail"
+              value={loginEmail}
+              onChange={handleInputChangeLoginForm}
+            />
+            <label for="emailLogin">Email address</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="passwordLogin"
+              placeholder="Password"
+              name="loginPassword"
+              value={loginPassword}
+              onChange={handleInputChangeLoginForm}
+            />
+            <label for="passwordLogin">Password</label>
+          </div>
 
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Repita la contraseña"
-                name="registerDuplicatedPassword"
-                value={registerDuplicatedPassword}
-                onChange={handleInputChangeRegisterForm}
-              />
-            </div>
-
-            <div className="form-group">
-              <input type="submit" className="btnSubmit" value="Crear cuenta" />
-            </div>
-          </form>
-        </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+        </form>
+        <p className="mt-3">
+          No yet member?{" "}
+          <button type="button" class="btn btn-link" onClick={handleSignupNow}>
+            Signup Now
+          </button>
+        </p>
+        <p className="mt-4 mb-3 text-muted">
+          By clicking Sign in, you agree to the terms of use.
+        </p>
       </div>
     </div>
   );
---
+++ src/components/auth/SignupScreen.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { authStartRegisterAction } from "../../actions/actions";
+import { useForm } from "../../hooks/useForm";
+
+export const SignupScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [registerFormValues, handleInputChangeRegisterForm] = useForm({
+    registerEmail: "",
+    registerName: "",
+    registerPassword: "",
+  });
+
+  const { registerEmail, registerName, registerPassword } = registerFormValues;
+
+  const handleOnSubmitRegister = (e) => {
+    e.preventDefault();
+    dispatch(
+      authStartRegisterAction(registerName, registerEmail, registerPassword)
+    );
+  };
+
+  const handleSigninNow = () => {
+    navigate("/login", {
+      replace: true,
+    });
+  };
+
+  return (
+    <>
+      <div className="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div className="row align-items-center g-lg-5 py-5">
+          <div className="col-lg-7 text-center text-lg-start">
+            <h1 className="display-4 fw-bold lh-1 mb-4">
+              {" "}
+              <i className="fas fa-shield-alt fa-lg"></i> Calendar App
+            </h1>
+            <p className="col-lg-10 fs-4">
+              Welcome to the top Marvel and DC Comics Super Heroes. <br />
+              Let's go and see more details about your favorite hero.
+            </p>
+          </div>
+          <div className="col-md-10 mx-auto col-lg-5">
+            <form
+              className="p-4 p-md-5 border rounded-3 bg-light"
+              onSubmit={handleOnSubmitRegister}
+            >
+              <div className="form-floating mb-3">
+                <input
+                  type="name"
+                  className="form-control"
+                  id="nameInputReg"
+                  placeholder="Your name"
+                  name="registerName"
+                  value={registerName}
+                  onChange={handleInputChangeRegisterForm}
+                />
+                <label for="nameInputReg">Your name</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="emailInputReg"
+                  placeholder="name@example.com"
+                  name="registerEmail"
+                  value={registerEmail}
+                  onChange={handleInputChangeRegisterForm}
+                />
+                <label for="emailInputReg">Email address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="passwordInputReg"
+                  placeholder="Password"
+                  name="registerPassword"
+                  value={registerPassword}
+                  onChange={handleInputChangeRegisterForm}
+                />
+                <label for="passwordInputReg">Password</label>
+              </div>
+
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Sign up
+              </button>
+
+              <hr className="my-4" />
+              <p>
+                Already a member?{" "}
+                <button
+                  type="button"
+                  class="btn btn-link"
+                  onClick={handleSigninNow}
+                >
+                  Signin Now
+                </button>
+              </p>
+
+              <small className="text-muted">
+                By clicking Sign up, you agree to the terms of use.
+              </small>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
